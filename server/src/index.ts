6902b62e4930ef12743f47dabf2392dfd62a6d34
Jordan Santoro <blvkrvbbit@gmail.com>
2026-04-28 19:20:59 -0600

    feat(auth-api): created sign up controller, and route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,30 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import authRoutes from "./routes/auth.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  "/api/*",
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/", (c) => {
+  return c.text("Bookmark Manager Api");
+});
+
+app.route("/api/v1/auth", authRoutes);
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
