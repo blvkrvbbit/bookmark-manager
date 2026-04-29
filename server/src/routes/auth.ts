97fd6fa048b8421dd891b1595f1bf60776685485
Jordan Santoro <blvkrvbbit@gmail.com>
2026-04-28 21:41:27 -0600

    feat(auth-api): hooked up sign-in route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 
-import { signUp } from "../controllers/auth.js";
+import { signIn, signUp } from "../controllers/auth.js";
 
 const authRoutes = new Hono();
 
 authRoutes.post("/sign-up", signUp);
+authRoutes.post("/sign-in", signIn);
 
 export default authRoutes;
